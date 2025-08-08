307348f4f338d5044b70d8b91a8fe6d27812c99a
Ivan Lorenzo <elhibar77@hotmail.es>
2025-08-08 15:09:51 +0200

    IO-5 feat: Moment Controller created

---
+++ src/main/java/dev/ivan/controllers/MomentController.java
@@ -0,0 +1,24 @@
+package dev.ivan.controllers;
+
+import dev.ivan.dtos.MomentDTO;
+import dev.ivan.mappers.MomentMapper;
+import dev.ivan.repositories.MomentRepository;
+import dev.ivan.models.Moment;
+
+
+
+
+public class MomentController {
+
+ private MomentRepository repository;
+
+ public MomentController() {
+  this.repository = new MomentRepository();
+ }
+
+public void StoreMoment(MomentDTO MomentDTO) {
+    Moment momentToSave = MomentMapper.toEntity(MomentDTO);
+    repository.StoreMoment(momentToSave);
+
+}
+}
