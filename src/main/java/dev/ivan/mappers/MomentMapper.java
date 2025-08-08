6ba797c119c9c92b1f8d77e20c6b92a9fc2e4904
Ivan Lorenzo <elhibar77@hotmail.es>
2025-08-08 14:29:57 +0200

    OPS-5 feat: Moment Mapper created to process the Moment DTO

---
+++ src/main/java/dev/ivan/mappers/MomentMapper.java
@@ -0,0 +1,14 @@
+package dev.ivan.mappers;
+
+import dev.ivan.dtos.MomentDTO;
+import dev.ivan.models.Moment;
+
+public class MomentMapper {
+
+ public static Moment toEntity(MomentDTO dto) {
+
+    Moment moment = new Moment(dto.title(), dto.date(), dto.description());
+    return moment;
+ }
+
+}
