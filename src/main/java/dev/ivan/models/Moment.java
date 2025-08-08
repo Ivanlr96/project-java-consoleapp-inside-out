f822a5c221af501f9cddf42f472682f2a3158bb3
Ivan Lorenzo <elhibar77@hotmail.es>
2025-08-08 14:13:22 +0200

    IO-5 feat: Moment model created

---
+++ src/main/java/dev/ivan/models/Moment.java
@@ -0,0 +1,29 @@
+package dev.ivan.models;
+
+public class Moment {
+
+    private String title;
+    private String date;
+    private String description;
+
+    public Moment(String title, String date, String description)
+    {
+        this.title = title;
+        this.date = date;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+}
