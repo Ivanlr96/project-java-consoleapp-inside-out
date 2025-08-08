0b235f1a047f0693ee6658bdeb23c734e41c8cb6
Ivan Lorenzo <elhibar77@hotmail.es>
2025-08-08 12:57:06 +0200

    IO-3 feat: menu created in the HomeView for the menu print

---
+++ src/main/java/dev/ivan/views/HomeView.java
@@ -0,0 +1,21 @@
+package dev.ivan.views;
+
+public class HomeView extends View{
+
+    public static void printMenu() {
+
+        String text = """
+                1. Añadir momento
+                2. Ver todos los momentos disponibles
+                3. Eliminar un momento
+                4. Filtrar los momentos
+                5. Salir
+                Seleccione una opción:
+                """;
+
+            System.out.println(text);
+
+            int option = scanner.nextInt();
+    }
+
+}
