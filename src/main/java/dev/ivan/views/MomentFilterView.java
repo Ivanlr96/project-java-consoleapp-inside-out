486a42f8b07e70a24d72494992435090799c13f7
Ivan Lorenzo <elhibar77@hotmail.es>
2025-08-13 14:50:11 +0200

    IO-16 feat: moment filter by emotion view created and previous menu created for the filter

--- src/main/java/dev/ivan/views/MomentFilterView.java
+++ src/main/java/dev/ivan/views/MomentFilterView.java
@@ -1,31 +1,23 @@
 package dev.ivan.views;
 
-import dev.ivan.controllers.MomentController;
-import dev.ivan.models.EmotionEnum;
-
 public class MomentFilterView extends View {
 
-    private static MomentController CONTROLLER = HomeView.momentController;
-
     public static void printFilterMenu() {
-        System.out.println("Seleccione la emoción para filtrar:");
-
-        for (EmotionEnum e : EmotionEnum.values()) {
-            System.out.println((e.ordinal() + 1) + ". " + e.getDisplayName());
-        }
+        System.out.println("Filtrar por ...:");
+        System.out.println("1. Emoción");
+        System.out.println("2. Fecha");
 
         int choice = SCANNER.nextInt();
 
-        if (choice < 1 || choice > EmotionEnum.values().length) {
-            System.out.println("Opción inválida.");
+        if (choice == 1) {
+            MomenFilterByEmotionView.printFilterMenu();
+        } else if (choice == 2) {
+
+        } else if (choice == 3) {
             HomeView.printMenu();
-            return;
+        } else {
+            System.out.println("Opción inválida. Intentelo de nuevo.");
+            printFilterMenu();
         }
-
-        EmotionEnum selectedEmotion = EmotionEnum.values()[choice - 1];
-
-        CONTROLLER.showMomentsByEmotion(selectedEmotion);
-
-        HomeView.printMenu();
     }
 }
