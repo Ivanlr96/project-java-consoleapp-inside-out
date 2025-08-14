b145721577d4cc14e58e07580f1e4c53c80469e0
Iván Lorenzo <elhibar77@hotmail.es>
2025-08-14 12:34:41 +0200

    IO-18 feat: test implemented for MomentMapper.java

---
+++ src/test/java/dev/ivan/mappers/MomentMapperTest.java
@@ -0,0 +1,29 @@
+package dev.ivan.mappers;
+
+import dev.ivan.dtos.MomentDTO;
+import dev.ivan.models.EmotionEnum;
+import dev.ivan.models.Moment;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class MomentMapperTest {
+
+    @Test
+    void testToEntityMapsCorrectly() {
+
+        LocalDate date = LocalDate.of(2025, 8, 14);
+        MomentDTO dto = new MomentDTO("Cumpleaños", date, "Mi cumpleaños", EmotionEnum.ALEGRIA);
+
+        Moment moment = MomentMapper.toEntity(dto);
+
+        assertThat(moment.getTitle(), is(dto.title()));
+        assertThat(moment.getDate(), is(dto.date()));
+        assertThat(moment.getDescription(), is(dto.description()));
+        assertThat(moment.getEmotionEnum(), is(dto.emotion()));
+     
+    }
+}
