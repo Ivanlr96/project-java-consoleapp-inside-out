f5ef75dece57ef0a42e6ba43a325e003eb570d11
Iván Lorenzo <elhibar77@hotmail.es>
2025-08-14 11:47:18 +0200

    IO-17 refact: implentend createdAt and ModifyAt

--- src/main/java/dev/ivan/models/Moment.java
+++ src/main/java/dev/ivan/models/Moment.java
@@ -1,6 +1,7 @@
 package dev.ivan.models;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Moment {
@@ -9,38 +10,34 @@ public class Moment {
     private LocalDate date;
     private String description;
     private EmotionEnum emotion;
+    private final LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public Moment(String title, LocalDate date, String description, EmotionEnum emotion) {
         this.title = title;
         this.date = date;
         this.description = description;
         this.emotion = emotion;
+        this.createdAt = LocalDateTime.now(); 
+        this.updatedAt = createdAt;          
     }
 
-    public String getTitle() {
-        return title;
-    }
+ 
+    public String getTitle() { return title; }
+    public LocalDate getDate() { return date; }
+    public String getDescription() { return description; }
+    public EmotionEnum getEmotionEnum() { return emotion; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public LocalDateTime getUpdatedAt() { return updatedAt; }
 
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public EmotionEnum getEmotionEnum() {
-        return emotion;
-    }
+    public void setUpdatedAt() { this.updatedAt = LocalDateTime.now(); }
 
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         return "Título: " + title + "\n" +
-                "Fecha: " + date.format(formatter) + "\n" +
-                "Descripción: " + description + "\n" +
-                "Emotion: " + emotion.getDisplayName() + "\n";
-
+               "Fecha: " + date.format(formatter) + "\n" +
+               "Descripción: " + description + "\n" +
+               "Emoción: " + emotion.getDisplayName() + "\n";
     }
-
 }
