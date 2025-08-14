a8d159e5b06dda7aed485881bfeecbb318194b77
Iván Lorenzo <elhibar77@hotmail.es>
2025-08-14 12:02:58 +0200

    IO-18 feat: test implemented for Diary Database

---
+++ src/test/java/dev/ivan/db/DiaryDatabaseTest.java
@@ -0,0 +1,58 @@
+package dev.ivan.db;
+
+import dev.ivan.models.EmotionEnum;
+import dev.ivan.models.Moment;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class DiaryDatabaseTest {
+
+    private DiaryDatabase db;
+
+    @BeforeEach
+    void setUp() {
+        db = new DiaryDatabase();
+    }
+
+    @Test
+    void testStoreAndGetAll() {
+        Moment moment = new Moment("Cumpleaños", LocalDate.of(2025, 8, 14), "Mi cumpleaños", EmotionEnum.ALEGRIA);
+        db.store(moment);
+
+        List<Moment> moments = db.getAll();
+
+
+        assertThat(moments, hasSize(1));
+        assertThat(moments.get(0).getTitle(), is("Cumpleaños"));
+        assertThat(moments.get(0).getEmotionEnum(), is(EmotionEnum.ALEGRIA));
+    }
+
+    @Test
+    void testDeleteValidIndex() {
+        Moment moment = new Moment("Cumpleaños", LocalDate.of(2025, 8, 14), "Mi cumpleaños", EmotionEnum.ALEGRIA);
+        db.store(moment);
+
+        boolean deleted = db.delete(0);
+
+        assertThat(deleted, is(true));
+        assertThat(db.getAll(), is(empty()));
+    }
+
+    @Test
+    void testDeleteInvalidIndex() {
+        boolean deleted = db.delete(5);
+        assertThat(deleted, is(false));
+    }
+
+    @Test
+    void testGetAllEmpty() {
+        List<Moment> moments = db.getAll();
+        assertThat(moments, is(empty()));
+    }
+}
